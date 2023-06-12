b30d19488e4e1d8d67e2178fbc5fb18af3c9c080
aCarinato <alessandro.carinato@gmail.com>
2023-06-12 12:24:56 +0200

    endpoint test 3

--- app.js
+++ app.js
@@ -33,47 +33,47 @@ app.use((req, res, next) => {
 //   (request, response) => {
 //     const event = request.body;
 
+// app.post(
+//   '/webhook',
+//   express.raw({ type: 'application/json' }),
+//   (request, response) => {
+//     let event = request.body;
+//     // Only verify the event if you have an endpoint secret defined.
+//     // Otherwise use the basic event deserialized with JSON.parse
+//     if (endpointSecret) {
+//       // Get the signature sent by Stripe
+//       const signature = request.headers['stripe-signature'];
+//       try {
+//         event = stripe.webhooks.constructEvent(
+//           request.body,
+//           signature,
+//           endpointSecret
+//         );
+//       } catch (err) {
+//         response
+//           .status(400)
+//           .send({ error: `Webhook Error: ${err.message}`, event: event });
+//         return;
+//         // console.log(`⚠️  Webhook signature verification failed.`, err.message);
+//         // return response.sendStatus(400);
+//       }
+//     }
+
 app.post(
   '/webhook',
   express.raw({ type: 'application/json' }),
   (request, response) => {
-    let event = request.body;
-    // Only verify the event if you have an endpoint secret defined.
-    // Otherwise use the basic event deserialized with JSON.parse
-    if (endpointSecret) {
-      // Get the signature sent by Stripe
-      const signature = request.headers['stripe-signature'];
-      try {
-        event = stripe.webhooks.constructEvent(
-          request.body,
-          signature,
-          endpointSecret
-        );
-      } catch (err) {
-        response
-          .status(400)
-          .send({ error: `Webhook Error: ${err.message}`, event: event });
-        return;
-        // console.log(`⚠️  Webhook signature verification failed.`, err.message);
-        // return response.sendStatus(400);
-      }
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      console.log(`Webhook Error: ${err.message}`);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
     }
-
-    // app.post(
-    //   '/webhook',
-    //   express.raw({ type: 'application/json' }),
-    //   (request, response) => {
-    //     const sig = request.headers['stripe-signature'];
-
-    //     let event;
-
-    // try {
-    //   event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    // } catch (err) {
-    //   console.log(`Webhook Error: ${err.message}`);
-    //   response.status(400).send(`Webhook Error: ${err.message}`);
-    //   return;
-    // }
 
     // Handle the event
     switch (event.type) {
